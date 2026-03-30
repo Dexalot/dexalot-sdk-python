919d268fd5e51832669b4155fc35618a7842ca56
M. Nihat Gurmen <mng@dexalot.com>
2026-03-30 16:41:18 -0500

    chore: Bump version to 0.5.2 and add version retrieval function

--- src/dexalot_sdk/__init__.py
+++ src/dexalot_sdk/__init__.py
@@ -1,7 +1,5 @@
 # dexalot_sdk - Dexalot Python SDK
 
-__version__ = "0.5.1"
-
 from .core.client import DexalotClient
 from .utils.cache import MemoryCache
 from .utils.secrets_vault import (
@@ -12,9 +10,18 @@ from .utils.secrets_vault import (
     secrets_vault_set,
 )
 
+__version__ = "0.5.2"
+
+
+def get_version() -> str:
+    """Return the current SDK version string."""
+    return __version__
+
+
 __all__ = [
     "DexalotClient",
     "MemoryCache",
+    "get_version",
     "generate_secrets_vault_key",
     "secrets_vault_get",
     "secrets_vault_list",
